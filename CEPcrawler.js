d77d5e723cc6667f702c469a3d761ba8c2da3676
Jose Neto <jose.neto@lavid.ufpb.br>
2018-10-11 16:54:22 -0300

    getting ready to read from sql db. still not needed

--- CEPcrawler.js
+++ CEPcrawler.js
@@ -1,7 +1,16 @@
-var fs = require('fs');
 
-(function IIFE(){
+(function IIFE(args){
     'use strict';
+
+    var fs = require('fs');
+    var mysql = require('mysql');
+
+    
+    args = args.slice(2);
+
+    var cepMode = args[0] || '-sql-off';
+
+    console.log(cepMode);
 
     'https://vip-pmeuinssprxr.inss.gov.br/apis/localizadorApsServices/buscaCep/08465312';
 
@@ -14,45 +23,29 @@ var fs = require('fs');
 
     var pageToVisit = 'https://vip-pmeuinssprxr.inss.gov.br/apis/localizadorApsServices/buscaCep/';
     //console.log("Visiting page " + pageToVisit);
-    var currCep = '00000000';
+    var currCep = '01000000';
     var iterator = 0;
     var dezena = 10;
     var recuo = 1;
 
     function iterateCEP(){
-        if (iterator < dezena){
-            currCep = currCep.slice(0, (currCep.length - recuo)) + iterator;
-            iterator += 1;
-        }else{
-            dezena *= 10;
-            recuo += 1;
+        if(cepMode === '-sql-off'){
+            if (iterator < dezena){
+                currCep = currCep.slice(0, (currCep.length - recuo)) + iterator;
+                iterator += 1;
+            }else{
+                dezena *= 10;
+                recuo += 1;
+            }
+        }
+        else{
+            console.log('to be done');
+            return;
         }
     }
 
     var listaCeps = new Array;
 
-    /*
-    request(pageToVisit + currCep, function(error, response, body) {
-        console.log('visitando pagina ' + (pageToVisit + currCep));
-        if(error) {
-            console.log("Error: " + error);
-        }
-        // Check status code (200 is HTTP OK)
-        //console.log("Status code: " + response.statusCode);
-        if(response.statusCode === 200) {
-            // Parse the document body
-            var $ = cheerio.load(body);
-            var result = JSON.parse($.text()).apsTO;
-            if(result.msgErro === undefined){
-                var ceps = result.apsTO;
-            }else{
-                console.log(result.msgErro);
-            }
-            console.log(ceps);
-        }
-        currCep += 1;
-    });            
-    //*/
     function search(url, callback){
         request(url, function(error, response, body) {
             console.log('visitando pagina ' + (url) + '\n');
@@ -66,9 +59,11 @@ var fs = require('fs');
                 var $ = cheerio.load(body);
                 var result = JSON.parse($.text()).apsTO;
                 if(result.msgErro === undefined){
-                    var ceps = result.apsTO;
+                    var ceps = result;
                     listaCeps.push(ceps);
-                    //console.log(listaCeps);
+                    console.log(ceps);
+                    fs.writeFileSync('ceps/cepINSS.json', JSON.stringify(listaCeps));
+
                 }
                 /*
                 else{
@@ -80,20 +75,21 @@ var fs = require('fs');
             }
             if(currCep != '99999999')
                 callback(); 
-            else
+            else{
                 console.log(listaCeps);
                 fs.writeFileSync('ceps/cepINSS.json', JSON.stringify(listaCeps));
                 console.log('finished');
-            
+            }
         });  
     }
 
     function crawl(){
         iterateCEP();
+        //currCep = '58052250'
         search(pageToVisit + currCep, crawl);
     }
 
     crawl();
 
 
-}())
+}(process.argv))
