39d1b623b1624e9d1a41ef1a27a7bc85aaff10f4
Jose Neto <jose.neto@lavid.ufpb.br>
2018-10-16 16:39:52 -0300

    some final changes

--- lua2json.lua
+++ lua2json.lua
@@ -1,7 +1,8 @@
+dofile('table.lua')
 local JSON = dofile('json.lua')
 
 local i = 1;
-
+--[[
 for i = 1, 11, 1 do
 
     print('transforming file', i)
@@ -10,6 +11,54 @@ for i = 1, 11, 1 do
     saida:write(JSON.stringify(input));
     saida:close();
     input = nil;
+    saida = nil;
 
 
 end
+
+--]]
+--[[
+for i = 1, 11, 1 do
+
+    print('transforming back file', i)
+    local Input = io.open('ceps/bloco_'..i..'.json', 'r');
+    local input = JSON.parse(Input:read("*all"));
+    local saida = io.open('cepBack/bloco_'..i..'.lua', 'w+');
+    saida:write('return '..table.tostring(input));
+    saida:close();
+    input = nil;
+    Input = nil;
+    saida = nil;
+
+
+end
+
+--]]
+
+
+--[[
+local teste = dofile('cepBack/bloco_1.lua');
+
+for i = 1, #teste, 1 do
+    print(teste[i].cep)
+end
+
+--]]
+
+
+
+---[[
+    
+
+print('transforming back file', i)
+local Input = io.open('ceps/cepINSS.json', 'r');
+local input = JSON.parse(Input:read("*all"));
+local saida = io.open('cepBack/cepINSS.lua', 'w+');
+saida:write('return '..table.tostring(input));
+saida:close();
+input = nil;
+Input = nil;
+saida = nil;
+
+
+--]]
